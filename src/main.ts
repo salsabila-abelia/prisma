464e8951f30a1d7772f07a60e668c66446163897
salsabila <salsabila_yocelyn_33rpl@student.smktelkom-mlg.sch.id>
2025-11-19 23:53:18 +0700

    update y

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,12 @@
+import 'dotenv/config';
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module'; // <--- Impor yang benar
 
 async function bootstrap() {
+  // opsional: cek apakah env terbaca
+  console.log('DEBUG DATABASE_URL:', process.env.DATABASE_URL);
+
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
